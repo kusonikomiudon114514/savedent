84f305ff4ca48ba3065ac182c2e833e63abf540d
Sakamoto <qbanben67@gmail.com>
2019-10-18 11:26:30 +0900

    えらーメッセージ

--- app/models/reportschool.rb
+++ app/models/reportschool.rb
@@ -6,4 +6,16 @@ class Reportschool < ApplicationRecord
   belongs_to :recommended
   belongs_to :user
 
+  validate :error_check
+
+  def error_check
+    if type_id.blank?
+      errors[:base]<<"学校種を選択してください"
+    end
+
+    if gakkou.blank?
+      errors[:base]<<"学校名を入力してください"
+    end
+  end  
+
 end
