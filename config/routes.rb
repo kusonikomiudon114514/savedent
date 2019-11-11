8943a106e42f0c62647e472af123dca6ff384bb6
Sakamoto <qbanben67@gmail.com>
2019-11-11 10:04:24 +0900

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,21 @@ Rails.application.routes.draw do
   get 'rirekisyo_tenso', to: 'rirekisyos#tenso'
   get 'home/top'
 
-  post '/reportjobs_field_search',to:'reportjobs#field_search'
+  post '/reportschools_place_search',to:'reportschools#place_search'							
+  get '/reportschools_place_search',to:'reportschools#index'																										
+  post '/reportschools_place_search',to:'reportschools#place_search'
+
+  post '/reportschools_type_search',to:'reportschools#type_search'							
+  get '/reportschools_type_search',to:'reportschools#index'																										
+  post '/reportschools_type_search',to:'reportschools#type_search'
+
+  post '/reportschools_field_search',to:'reportschools#field_search'							
+  get '/reportschools_field_search',to:'reportschools#index'																										
+  post '/reportschools_field_search',to:'reportschools#field_search'
+
+  post '/reportschools_search',to:'reportschools#search'							
+  get '/reportschools_search',to:'reportschools#index'																										
+  post '/reportschools_search',to:'reportschools#search'
 
   devise_for :users
   resources :teachers
