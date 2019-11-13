f5d13f788209919aa7edc5c63adda7598214f771
3415 <3415@e-mail.com>
2019-11-13 14:46:04 +0900

    競合解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-<<<<<<< HEAD
 
 
   private
@@ -30,23 +29,4 @@ class ApplicationController < ActionController::Base
      end 
  
       
-=======
-  before_action :check_username
-
-private
-def check_username
-   if current_user.nil? || controller_name == "students" || controller_name == "sessions"
-   else
-     students = Student.where(user_id: current_user.id)
-     if students.blank?
-       redirect_to new_student_path, notice: '名前を入力してください。' 
-     end
-   end
-end
-
-
-
-
-
->>>>>>> fa50b5c99a968369ddb2b472d74efa347c6b9df3
 end
