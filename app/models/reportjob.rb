af7fb5e37e186d8cedde9eec91f551eb8938da02
Sayuka <haradasayuka@email.com>
2020-01-15 15:03:15 +0900

    a

--- app/models/reportjob.rb
+++ app/models/reportjob.rb
@@ -2,6 +2,9 @@ class Reportjob < ApplicationRecord
 
   belongs_to :user, optional: true
   has_many :rjlogs, :dependent => :destroy
+  belongs_to :field
+  belongs_to :mediation
+  belongs_to :interview
 
   #validate :error_check
 
